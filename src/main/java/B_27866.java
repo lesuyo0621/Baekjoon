dc8b568fa35114d7dfd4cf8e911126ca2487609d
lesuyo0621 <lesuyo930621@naver.com>
2024-06-10 03:12:39 +0900

    feat : 27866 번 문제 추가
    - https://www.acmicpc.net/problem/27866

---
+++ src/main/java/B_27866.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class B_27866 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.nextLine();
+        int i = sc.nextInt() - 1;
+
+        System.out.println(s.charAt(i));
+    }
+}
